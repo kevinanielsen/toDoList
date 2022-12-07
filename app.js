443f57edb0597cc8a8d33f996a9d06cecb0be418
Kevin Nielsen <keveran@gmail.com>
2022-12-07 20:09:44 +0100

    Added styling and new task functionality

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-let addTaskButton = document.getElementById('todo-input');
-
+let addTaskButton = document.getElementById('add-button');
+let i = 0;
 const addTask = () => {
+    i++;
+    let newItem = document.createElement('li');
+    let doneButton = document.createElement('button');
+    doneButton.innerHTML = 'Done';
+    newItem.innerHTML = 'Hej';
+    newItem.appendChild(doneButton);
+    doneButton.setAttribute('class', 'done-button');
+    document.getElementById('todo').appendChild(newItem);
+}
+
+const moveTask = () => {
     
-};
+}
+addTaskButton.addEventListener('click', addTask)
+document.getElementsByClassName[i].addEventListener('click', )
