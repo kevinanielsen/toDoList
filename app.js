21de414c6cff8b7c1203af44a9887c75cfb83c7c
Kevin Nielsen <keveran@gmail.com>
2022-12-11 19:51:52 +0100

    Added local storage functionality

--- app.js
+++ app.js
@@ -1,22 +1,50 @@
 let addTaskButton = document.getElementById('add-button');
-let i = 0;
 let currentId = '';
+let currentTasks = [];
+let doneTasks = [];
+let j = 0;
 
-const addTask = () => {
-    i++;
-    let newItem = document.createElement('li');
-    let doneButton = document.createElement('button');
-    doneButton.innerHTML = 'Done';
-    newItem.innerHTML = 'Hej';
-    newItem.appendChild(doneButton);
-    doneButton.setAttribute('id', 'done-button' + i);
-    document.getElementById('todo').appendChild(newItem);
-    doneButton.addEventListener('click', moveTask);
-}
+let storedTasks = localStorage.currentTasks;
+let storedTasksArr = storedTasks.split(',');
 
 const moveTask = (event) => {
     event.target.style.display = 'none';
     document.getElementById('done').appendChild(event.target.parentElement);
+    localStorage.removeItem(currentTasks[event.target.value]);
+
 }
-addTaskButton.addEventListener('click', addTask)
+
+for (let i = 0; i < storedTasksArr.length; i++) {
+    let storedItem = document.createElement('li');
+    let doneButton = document.createElement('button');
+    storedItem.innerHTML = storedTasksArr[i];
+    storedItem.appendChild(doneButton);
+    doneButton.setAttribute('id', 'done-button' + i);
+    doneButton.innerHTML = 'Done';
+    document.getElementById('todo').appendChild(storedItem);
+    doneButton.addEventListener('click', moveTask);
+    currentTasks.push(storedTasksArr[i]);
+}
+
+const addTask = () => {
+    j++;
+    let newItem = document.createElement('li');
+    let doneButton = document.createElement('button');
+    let inputData = document.getElementById('todo-input').value;
+    
+    newItem.innerHTML = inputData;
+    newItem.appendChild(doneButton);
+    doneButton.setAttribute('id', 'done-button' + j);
+    document.getElementById('todo').appendChild(newItem);
+    doneButton.addEventListener('click', moveTask);
+    currentTasks.push(inputData);
+
+    doneButton.innerHTML = 'Done';
+
+    localStorage.setItem('currentTasks', currentTasks);
+    console.log(localStorage);
+}
+
+
+addTaskButton.addEventListener('click', addTask);
 
